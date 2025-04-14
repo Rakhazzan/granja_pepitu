fdbcd74aea58898de4a52d130c0ed82d2459c633
Rakhazzan <makhazzan@inscastellet.cat>
2025-04-14 15:04:49 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'granja_pepitu',
+  webDir: 'dist'
+};
+
+export default config;
