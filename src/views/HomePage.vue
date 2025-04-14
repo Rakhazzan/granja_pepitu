d19d2a054b604c694b43c49cb30ffc5f90c52e22
Rakhazzan <makhazzan@inscastellet.cat>
2025-04-14 15:15:36 +0200

    a

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,56 +1,44 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header>
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>La granja d'en Pepitu</ion-title>
       </ion-toolbar>
     </ion-header>
 
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content>
+      <ion-list>
+        <ion-item v-for="animal in animals" :key="animal.nom">
+          <ion-img :src="animal.img" style="width: 100px; height: 100px;"></ion-img>
+          <ion-label class="ion-text-wrap">
+            <h2>{{ animal.nom }}</h2>
+          </ion-label>
+          <ion-button @click="playSound(animal.soroll)">🐾 Escolta</ion-button>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script setup>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonImg, IonLabel, IonButton } from '@ionic/vue'
+import { Howl } from 'howler'
+
+const animals = [
+  { nom: 'Vaca', img: 'assets/animals/vaca.jpg', soroll: 'assets/sounds/vaca.mp3' },
+  { nom: 'Gall', img: 'assets/animals/gall.jpg', soroll: 'assets/sounds/gall.mp3' },
+  { nom: 'Porc', img: 'assets/animals/porc.jpg', soroll: 'assets/sounds/porc.mp3' },
+  { nom: 'Ovella', img: 'assets/animals/ovella.jpg', soroll: 'assets/sounds/ovella.mp3' },
+  { nom: 'Ànec', img: 'assets/animals/anec.jpg', soroll: 'assets/sounds/anec.mp3' },
+  { nom: 'Gat', img: 'assets/animals/gat.jpg', soroll: 'assets/sounds/gat.mp3' },
+  { nom: 'Gos', img: 'assets/animals/gos.jpg', soroll: 'assets/sounds/gos.mp3' },
+  { nom: 'Cavall', img: 'assets/animals/cavall.jpg', soroll: 'assets/sounds/cavall.mp3' },
+  { nom: 'Gallina', img: 'assets/animals/gallina.jpg', soroll: 'assets/sounds/gallina.mp3' },
+  { nom: 'Oca', img: 'assets/animals/oca.jpg', soroll: 'assets/sounds/oca.mp3' },
+]
+
+function playSound(url) {
+  const sound = new Howl({ src: [url] })
+  sound.play()
+}
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
